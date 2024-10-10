e581109fc9077ec8a59a39c74457e089ed13aa54
Nelson <n.ortizf@uniandes.edu.co>
2024-10-10 11:07:50 -0500

    creacion de modelos y primer makemigrations

--- src/incidentes/models.py
+++ src/incidentes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class IncidenteCorreo(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    revisado = models.BooleanField(default=False)
+    correoUsuario = models.CharField(max_length=20)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+
+class Incidente(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    numeroDocumento = models.IntegerField()
+    tipoDocumento = models.TextField()
+    nombreUsuario = models.CharField(max_length=100)
+    empresaAsociada = models.CharField(max_length=100)
+    correo = models.CharField(max_length=100)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    planEmpresarial = models.CharField(max_length=100)
