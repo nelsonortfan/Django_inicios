075c9f753282570dad67905f76cf99046c38c0c4
Nelson <n.ortizf@uniandes.edu.co>
2024-10-11 12:08:48 -0500

    agregando codes status en respuestas

--- src/incidentes/views.py
+++ src/incidentes/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from incidentes.serializers import IncidenteCorreoSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 
 def index(request):
@@ -14,31 +15,40 @@ def incidente_correo_lista(request):
     if request.method == 'GET':
         incidentesCorreo = IncidenteCorreo.objects.all()
         serializer = IncidenteCorreoSerializer(incidentesCorreo, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == 'POST':
         serializer = IncidenteCorreoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','PUT','DELETE'])
 def incidente_correo_detalle(request, pk):
     if request.method == 'GET':
-        incidente_correo = IncidenteCorreo.objects.get(id=pk)
+        try:
+            incidente_correo = IncidenteCorreo.objects.get(id=pk)
+        except IncidenteCorreo.DoesNotExist:
+            return Response({'error':'Correo con Incidente no encontrado'}, status=status.HTTP_404_NOT_FOUND)
         serializer = IncidenteCorreoSerializer(incidente_correo)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == 'PUT':
-        incidente_correo = IncidenteCorreo.objects.get(id=pk)
+        try:
+            incidente_correo = IncidenteCorreo.objects.get(id=pk)
+        except IncidenteCorreo.DoesNotExist:
+            return Response({'error':'Correo con Incidente no encontrado'}, status=status.HTTP_404_NOT_FOUND)
         serializer = IncidenteCorreoSerializer(incidente_correo, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors) 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
     if request.method == 'DELETE':
-        incidente_correo = IncidenteCorreo.objects.get(id=pk)
+        try:
+            incidente_correo = IncidenteCorreo.objects.get(id=pk)
+        except IncidenteCorreo.DoesNotExist:
+            return Response({'error':'Correo con Incidente no encontrado'}, status=status.HTTP_404_NOT_FOUND)
         incidente_correo.delete()
-        return Response("Elemento eliminado correctamente")   
+        return Response(status=status.HTTP_204_NO_CONTENT)   
