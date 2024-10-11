fc23a8f9f98d43d3d2f08a56ceb922c6d955a730
Nelson <n.ortizf@uniandes.edu.co>
2024-10-11 13:02:21 -0500

    removiendo validacion del correo y usando el campo modelserializers

--- src/incidentes/serializers.py
+++ src/incidentes/serializers.py
@@ -13,8 +13,3 @@ class IncidenteCorreoSerializer(serializers.ModelSerializer):
         else:
             return value
         
-    def validate_correoUsuario(self, value):
-        if str(value).__contains__('@') and str(value).count('@') == 1:
-            return value
-        else:
-            raise serializers.ValidationError('Ingrese un correo valido')
